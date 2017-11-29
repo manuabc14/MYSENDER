a82774658e251d1d420250b5e90ce6dc51a70328
entahinta <emmanuel.ntahinta@trsb.net>
2017-11-30 00:22:49 +0100

    chore: AJout du controleur calendrier

---
+++ MYSENDER/Controllers/CalendarController.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MYSENDER.Controllers
+{
+    public class CalendarController : Controller
+    {
+        // GET: Calendar
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Calendar/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Calendar/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Calendar/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Calendar/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Calendar/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Calendar/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Calendar/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- MYSENDER/wwwroot/js/js-planingpersonnel.js
+++ MYSENDER/wwwroot/js/js-planingpersonnel.js
@@ -1,29 +1,33 @@
 ﻿$(document).ready(function () {
-    $("#CalendarPlaning").click(function () {
 
-        location.href = $("#planing").data("url-list-personnel");
+    $('#calendar').fullCalendar({
+        // put your options and callbacks here
+    })
+    //$("#CalendarPlaning").click(function () {
 
-    });
+    //    location.href = $("#planing").data("url-list-personnel");
 
-    /* initialize the external events
-         -----------------------------------------------------------------*/
-    var event = $("#planing").data("model");
-    event.forEach(function (e) {
-        e.start = moment(e.start, "DD/MM/YYYY hh:mm").format("YYYY-MM-DD hh:mm");
-        e.end = moment(e.end, "DD/MM/YYYY hh:mm").format("YYYY-MM-DD hh:mm");
-        e.end = moment(e.end).add(1, 'days'); // pour afficher les date de fin inclusive.
-    });
-    /* initialize the calendar
-    -----------------------------------------------------------------*/
-    var cal = $('#calendar').fullCalendar({
-        header: {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'month,agendaWeek,agendaDay'
-        },
-        editable: true,
-        droppable: true,
-        events: event
-    });
+    //});
+
+    ///* initialize the external events
+    //     -----------------------------------------------------------------*/
+    //var event = $("#planing").data("model");
+    //event.forEach(function (e) {
+    //    e.start = moment(e.start, "DD/MM/YYYY hh:mm").format("YYYY-MM-DD hh:mm");
+    //    e.end = moment(e.end, "DD/MM/YYYY hh:mm").format("YYYY-MM-DD hh:mm");
+    //    e.end = moment(e.end).add(1, 'days'); // pour afficher les date de fin inclusive.
+    //});
+    ///* initialize the calendar
+    //-----------------------------------------------------------------*/
+    //var cal = $('#calendar').fullCalendar({
+    //    header: {
+    //        left: 'prev,next today',
+    //        center: 'title',
+    //        right: 'month,agendaWeek,agendaDay'
+    //    },
+    //    editable: true,
+    //    droppable: true,
+    //    events: event
+    //});
 
 });
